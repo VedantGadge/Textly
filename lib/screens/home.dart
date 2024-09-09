f2fc263bf789b63188776537cb4dfea7c7f98068
Vedant Gadge <vedant.gadgegsis@gmail.com>
2024-09-09 10:43:08 +0530

    .

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,7 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         //AppBar
         appBar: AppBar(
-          title: const Text('Textl'),
+          title: const Text('Textly'),
           leading: const Icon(
             CupertinoIcons.home,
             color: Colors.white,
